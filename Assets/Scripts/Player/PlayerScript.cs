10876a631858c7b1a03464e415b4077623bbf11e
antoniaonisoru <82662300+antoniaonisoru@users.noreply.github.com>
2023-07-06 16:09:11 +0300

    Assasin

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -11,6 +11,13 @@ public class PlayerScript : MonoBehaviour
 
 
     public Animator animator;
+
+    public int damagePerHit;
+    public int damage;
+
+    public float secondsCoolDown;
+    public float secondsAttackAnimation;
+
     void Start()
     {
         noHearts = 3;
@@ -20,10 +27,15 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(StopAndAttack(secondsAttackAnimation));
+        }
     }
 
     public void TakeDamage( int damage )
     {
+        Debug.Log("pula");
         animator.SetInteger("State", 4);
         //Debug.Log(damage);
         if (health - damage > 0)
@@ -35,6 +47,16 @@ public class PlayerScript : MonoBehaviour
             noHearts--;
         }
         animator.SetInteger("State", 0);
+    }
+
+    IEnumerator StopAndAttack(float seconds)
+    {
+        animator.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(seconds);
+
+        animator.SetInteger("State", 1);
+
     }
 
 }
