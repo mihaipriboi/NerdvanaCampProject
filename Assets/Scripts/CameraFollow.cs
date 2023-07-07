158421bf7ecc565b9a830afa5f798fc8d4bfe95f
teodorbalan <82662085+teodorbalan@users.noreply.github.com>
2023-07-07 12:42:56 +0300

    First Level Update

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,21 +10,34 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private float cameraLeft;
     [SerializeField] private float cameraRight;
 
+    private CinemachineVirtualCamera virtualCamera;
+    private float offsetY = 0f;
+
+
     void Start()
     {
-        
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (virtualCamera.m_Lens.OrthographicSize > 7.5)
+        {
+            offsetY = -2f;
+        } else
+        {
+            offsetY = 2f;
+        }
+
         if (transform.position.y > cameraTop)
         {
             GetComponent<CinemachineCameraOffset>().m_Offset.y = -transform.position.y + cameraTop;
         } 
         else if (transform.position.y < cameraBottom)
         {
-            GetComponent<CinemachineCameraOffset>().m_Offset.y = -transform.position.y + cameraBottom;
+            GetComponent<CinemachineCameraOffset>().m_Offset.y = -transform.position.y + (cameraBottom - offsetY);
         }
     }
 }
